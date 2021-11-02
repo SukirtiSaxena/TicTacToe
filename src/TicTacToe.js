6586d10d3efdc892d169db6e2ce17538537d6a73
SukirtiSaxena <91497529+SukirtiSaxena@users.noreply.github.com>
2021-11-02 21:54:56 +0000

    refactored code
    refactored code

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,42 +1,37 @@
 /**
-  * 
-  * [ [1, 2, 3],
-  *   [4, 5, 6],
-  *   [7, 8, 9] ]
-  * The function should return "X Wins " if player X has won, "0 Wins" if the player 0 has won, and 'No Winner' if there is currently no winner.
+  * ## Tic Tac Toe Game
   */
 
 
-const ticTacToeGame = (moveX, move0) => {
+ const ticTacToeGame = (moveX, move0) => {
    let board = [
       [null, null, null],
       [null, null, null],
       [null, null, null]
    ];
    let winner = "Still Not Decided";
-   let isGameOver = false;
+   if ((moveX.flat().filter(x => x < 0 || x > 2)).length > 0 || (move0.flat().filter(x => x < 0 || x > 2)).length > 0)
+      return "Input should be within the board";
+
    for (let i = 0; i < moveX.length; i++) {
-      if ((moveX[i][0] > 2) || (moveX[i][0] < 0) || (moveX[i][1] > 2) || (moveX[i][1] < 0))
-         return "Input should be within the board";
-      if ((i < move0.length) && ((move0[i][0] > 2) || (move0[i][0] < 0) || (move0[i][1] > 2) || (move0[i][1] < 0)))
-         return "Input should be within the board";
-
       if (winner = "Still Not Decided") {
-         let x = moveX[i][0];
-         let y = moveX[i][1];
-         board[x][y] = "X";
-         winner = findWinner(board);
-         if (winner === "X" || winner === "0") return winner; 
+         winner = movesOnBoard(moveX[i][0], moveX[i][1], "X", board);
+         if (winner === "X" || winner === "0" || winner === "No Winner in this game") return winner;
 
-         let a = move0[i][0];
-         let b = move0[i][1];
-         board[a][b] = "0";
-         winner = findWinner(board);
-         if (winner === "X" || winner === "0") return winner; 
+         winner = movesOnBoard(move0[i][0], move0[i][1], "0", board);
+         if (winner === "X" || winner === "0" || winner === "No Winner in this game") return winner;
       }
       else
          return winner;
    };
+};
+
+const movesOnBoard = (row, column, move, board) => {
+   let x = row;
+   let y = column;
+   board[x][y] = move;
+   console.log("board",board);
+   return findWinner(board);
 };
 
 const findWinner = (board) => {
@@ -45,41 +40,30 @@ const findWinner = (board) => {
       ((board[0][0] === board[0][1] && board[0][0] === board[0][2]) ||
          (board[0][0] === board[1][1] && board[0][0] === board[2][2]) ||
          (board[0][0] === board[1][0] && board[0][0] === board[2][0]))) 
-  
       return board[0][0];
 
 
    if ((board[0][2] !== null) &&
       ((board[0][2] === board[1][2] && board[0][2] === board[2][2]) ||
-         (board[0][2] === board[1][1] && board[0][2] === board[2][0]))) 
-    
+         (board[0][2] === board[1][1] && board[0][2] === board[2][0])))    
       return board[0][2];
-   
-
 
    if (board[0][1] !== null && board[0][1] === board[1][1] && board[0][1] === board[2][1]) 
-
       return board[0][1];
    
-
    else
-      if (board[1][0] !== null && board[1][0] === board[1][1] && board[1][0] === board[1][2]) 
-    
+      if (board[1][0] !== null && board[1][0] === board[1][1] && board[1][0] === board[1][2])   
          return board[1][0];
       
-
       else
-         if (board[2][0] !== null && board[2][0] === board[2][1] && board[2][0] === board[2][2]) 
-   
+         if (board[2][0] !== null && board[2][0] === board[2][1] && board[2][0] === board[2][2])   
             return board[2][0];
          
          else
             if (board.some(row => row.includes(null)))
                return "Still Not Decided";
-            else 
-           
-               return "No Winner in this game";
-            
+            else           
+               return "No Winner in this game";         
 };
 
 module.exports = { ticTacToeGame, findWinner };
--- src/TicTacToe.test.js
+++ src/TicTacToe.test.js
@@ -1,34 +1,46 @@
-const  { ticTacToeGame , findWinner }  = require("./TicTacToe.js");
+const  {ticTacToeGame} = require("./TicTacToe.js");
 
 describe("ticTacToeGame", () => {
-    test(" Retun winner of the game ", () => {
-        const moveX = [ [0,0], [0,1], [0,2] ];
-        const move0 = [ [2,1],[2,2]];
-        expect(ticTacToeGame(moveX,move0)).toEqual("X");
-     });
+   test(" Return 'X' if winner of the game is X", () => {
+      const moveX = [[0, 0], [0, 1], [0, 2]];
+      const move0 = [[2, 1], [2, 2]];
+      expect(ticTacToeGame(moveX, move0)).toEqual("X");
+   });
 
-     test(" if X input value is > 2 then error message 'Input should be within the board' ", () => {
-        const moveX = [[4,0]];
-        const move0 = [[2,1]];
-        expect(ticTacToeGame(moveX,move0)).toEqual("Input should be within the board");
-     });
+   test(" Return '0' if winner of the game is 0 ", () => {
+      const moveX = [[0, 0], [0, 1], [1, 2]];
+      const move0 = [[0, 2], [1, 1], [2, 0]];
+      expect(ticTacToeGame(moveX, move0)).toEqual("0");
+   });
 
-     test("  if X input value is < 0 then error message 'Input should be within the board'", () => {
-        const moveX = [[2,-2]];
-        const move0 = [[2,1]];
-        expect(ticTacToeGame(moveX,move0)).toEqual("Input should be within the board");
-     });
+   test(" Return 'No Winner in this game' if there is no winner in the game ", () => {
+      const moveX = [[0,1],[1,0],[1,2],[2,0],[2,2]];
+      const move0 = [[0,0],[0,2],[1,1],[2,1]];
+      expect(ticTacToeGame(moveX, move0)).toEqual("No Winner in this game");
+   });
 
-     test(" if 0 input value is > 2 then error message 'Input should be within the board' ", () => {
-        const moveX = [[2,0]];
-        const move0 = [[6,1]];
-        expect(ticTacToeGame(moveX,move0)).toEqual("Input should be within the board");
-     });
+   test(" if X input value is > 2 then error message 'Input should be within the board' ", () => {
+      const moveX = [[4, 0]];
+      const move0 = [[2, 1]];
+      expect(ticTacToeGame(moveX, move0)).toEqual("Input should be within the board");
+   });
 
-     test("  if 0 input value is < 0 then error message 'Input should be within the board'", () => {
-        const moveX = [[2,2]];
-        const move0 = [[2,-1]];
-        expect(ticTacToeGame(moveX,move0)).toEqual("Input should be within the board");
-     });
+   test("  if X input value is < 0 then error message 'Input should be within the board'", () => {
+      const moveX = [[2, -2]];
+      const move0 = [[2, 1]];
+      expect(ticTacToeGame(moveX, move0)).toEqual("Input should be within the board");
+   });
 
-    });
+   test(" if 0 input value is > 2 then error message 'Input should be within the board' ", () => {
+      const moveX = [[2, 0]];
+      const move0 = [[6, 1]];
+      expect(ticTacToeGame(moveX, move0)).toEqual("Input should be within the board");
+   });
+
+   test("  if 0 input value is < 0 then error message 'Input should be within the board'", () => {
+      const moveX = [[2, 2]];
+      const move0 = [[2, -1]];
+      expect(ticTacToeGame(moveX, move0)).toEqual("Input should be within the board");
+   });
+
+});
